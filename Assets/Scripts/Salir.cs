5a50f9580544bd2f5536ac2787740ccaf63c8d35
Manuela Recio <manu138@hotmail.es>
2016-11-14 15:18:24 -0500

    Listo todo

--- Assets/Scripts/Salir.cs
+++ Assets/Scripts/Salir.cs
@@ -3,25 +3,28 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 // Classe que contém a função OnClick(), que é chamada ao clicarmos no botão
-public class MenuController : MonoBehaviour {
+public class Salir : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+    // Use this for initialization
+    void Start()
+    {
+        if (Input.GetKey("escape"))
             Application.Quit();
-        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
     // A função OnClick() troca de cena
     // Lembrem que adicionamos as cenas que nosso jogo conterá em File->Build Settings
-    public void OnClick(){
-        SceneManager.LoadScene(1);
+    public void ClickExit()
+
+    {
+        Application.Quit();
     }
 
 }
--- Assets/Scripts/turncontroller.cs
+++ Assets/Scripts/turncontroller.cs
@@ -9,13 +9,20 @@ public class turncontroller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 
-	}
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+    }
 }
